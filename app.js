60e9768caf25517aeb437b1de7f607ae5baa398c
BooMajka <80685266+BooMajka@users.noreply.github.com>
2022-01-04 19:20:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+// const profileDataArgs = process.argv.slice(2);
+// const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
+
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 const { writeFile, copyFile } = require('./utils/generate-site');
